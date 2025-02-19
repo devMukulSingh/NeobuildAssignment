df7eaf8715d15712f9826bd8c6ca2ae4088367f5
devMukulSingh <mukulsingh2276@gmail.com>
2025-02-19 21:06:31 +0530

    added login api

--- src/routes/authApp.ts
+++ src/routes/authApp.ts
@@ -3,25 +3,26 @@ import { userSchema } from "../lib/schema.js";
 import { prisma } from "../lib/constants.js";
 import { Prisma } from "@prisma/client";
 import bcrypt from "bcrypt"
+import { jwtSign } from "../lib/helpers.js";
 
 const authApp = new Hono();
 
-authApp.post('/register', async(c) => {
+authApp.post('/register', async (c) => {
     const body = await c.req.json();
     const parsedBody = userSchema.safeParse(body);
-    
-    if(!parsedBody.success){
+
+    if (!parsedBody.success) {
         return c.json({
-            error:parsedBody.error.errors.map(err => err.message)
-        },400)
+            error: parsedBody.error.errors.map(err => err.message)
+        }, 400)
     }
-    
-    const { email,name,password } = parsedBody.data
-    
-    try{
-        const hashedPassword = await bcrypt.hash(password,12);
+
+    const { email, name, password } = parsedBody.data
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = await prisma.user.create({
-            data:{
+            data: {
                 email,
                 name,
                 password: hashedPassword
@@ -29,22 +30,66 @@ authApp.post('/register', async(c) => {
         })
 
         return c.json({
-            msg:"User registered successfully",
-        },201)
+            msg: "User registered successfully",
+        }, 201)
 
     }
-    catch(e){
+    catch (e) {
         console.log(e);
-        if(e instanceof Prisma.PrismaClientKnownRequestError){
-            if(e.code==="P2002"){
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2002") {
                 return c.json({
-                    error:"User already exists"
-                },409)
+                    error: "User already exists"
+                }, 409)
             }
         }
         return c.json({
-            error:"Internal server error " + e
-        },500)
+            error: "Internal server error " + e
+        }, 500)
+    }
+})
+
+
+authApp.post('/login', async (c) => {
+
+    const body = await c.req.json();
+    const parsedBody = userSchema.pick({ email: true, password: true }).safeParse(body);
+    if (!parsedBody.success) {
+        return c.json({
+            error: parsedBody.error.errors.map(err => err.message),
+        }, 400)
+    }
+    const { email, password } = parsedBody.data;
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+        if (!user) {
+            return c.json({
+                error: "Invalid credentials"
+            }, 400)
+        }
+
+        if (!await bcrypt.compare(password, user?.password)) {
+            return c.json({
+                error: "Invalid credentials"
+            }, 400)
+        }
+
+        const jwt = await jwtSign()
+
+        return c.json({
+            JWT: jwt
+        },200)
+    }
+
+    catch (e) {
+        console.log(e);
+        return c.json({
+            error: "Internal server error " + e
+        })
     }
 })
 
